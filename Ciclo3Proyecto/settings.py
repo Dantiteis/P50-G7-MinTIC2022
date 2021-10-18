3853ef04fe0a5e5fe2780f3e91729e22efc1185a
Dantiteis <dantiteis@gmail.com>
2021-10-17 19:04:43 -0500

    uncomment django_heroku

--- Ciclo3Proyecto/settings.py
+++ Ciclo3Proyecto/settings.py
@@ -155,6 +155,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#import django_heroku
-#django_heroku.settings(locals())
+import django_heroku
+django_heroku.settings(locals())
 
